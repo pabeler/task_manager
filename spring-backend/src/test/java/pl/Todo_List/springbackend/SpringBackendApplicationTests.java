080bd5ccaf0f0c3f2e6d37028de04bdae67976da
pabeler <pabeler@o2.pl>
2023-06-21 09:08:24 +0200

    added folders with spring boot and react

---
+++ spring-backend/src/test/java/pl/Todo_List/springbackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Todo_List.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
