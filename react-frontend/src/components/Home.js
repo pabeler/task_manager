7ba24d5453e35f4e25c62ec564ba872f1cec15e8
pabeler <pabeler@o2.pl>
2023-06-22 16:34:01 +0200

    Add content to home page

--- react-frontend/src/components/Home.js
+++ react-frontend/src/components/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Text} from "@nextui-org/react";
+import Container from "react-bootstrap/Container";
 
 export default function Home() {
     return (
         <>
-            <h1>Home</h1>
+            <Container style={{minHeight: "80vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+                <Text h1 size={60} css={{textGradient: "45deg, $blue600 -20%, $pink600 50%",}} weight="bold">
+                    Discover&nbsp;
+                </Text>
+                <Text h1 size={60} css={{textGradient: "45deg, $purple600 -20%, $pink600 100%",}} weight="bold">
+                    our task&nbsp;
+                </Text>
+                <Text h1 size={60} css={{textGradient: "45deg, $yellow600 -20%, $red600 100%",}} weight="bold">
+                    manager
+                </Text>
+            </Container>
         </>
     );
 }
