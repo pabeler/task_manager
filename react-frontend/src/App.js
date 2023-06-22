931019ca0ac4ab6c23b37131ff4eaa01640e9b92
pabeler <pabeler@o2.pl>
2023-06-22 16:40:52 +0200

    Added custom footer

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from "./components/Home";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import CustomNavbar from "./components/CustomNavbar";
+import CustomFooter from "./components/CustomFooter";
 
 function App() {
 return (
@@ -11,6 +12,7 @@ return (
             <Routes>
                 <Route path="/" element={<Home/>}/>
             </Routes>
+            <CustomFooter/>
         </BrowserRouter>
     </div>
 );
---
+++ react-frontend/src/components/CustomFooter.js
@@ -0,0 +1,11 @@
+export default function CustomFooter() {
+    return (
+        <>
+            <footer className="footer">
+                <div className="container">
+                    <span className="text-muted">Created by Paul Olejnik 2023</span>
+                </div>
+            </footer>
+        </>
+    );
+}
