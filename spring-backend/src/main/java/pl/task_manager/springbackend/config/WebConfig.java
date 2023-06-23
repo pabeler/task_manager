8e925160581eebd4422abd368a2460d3f35a71f8
pabeler <pabeler@o2.pl>
2023-06-23 04:05:33 +0200

    Added filter responsible for incrementing the request counter

---
+++ spring-backend/src/main/java/pl/task_manager/springbackend/config/WebConfig.java
@@ -0,0 +1,19 @@
+package pl.task_manager.springbackend.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pl.task_manager.springbackend.filter.RequestCounterFilter;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean<RequestCounterFilter> loggingFilter(){
+        FilterRegistrationBean<RequestCounterFilter> registrationBean
+                = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestCounterFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+}
+
---
+++ spring-backend/src/main/java/pl/task_manager/springbackend/filter/RequestCounterFilter.java
@@ -0,0 +1,30 @@
+package pl.task_manager.springbackend.filter;
+
+import jakarta.servlet.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class RequestCounterFilter implements Filter {
+    private int requestCount;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+        requestCount = 0;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        requestCount++;
+        log.info("Request number: " + requestCount);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {}
+}
