58be4546a388555906e6b95d926328d3dce02ef0
pabeler <pabeler@o2.pl>
2023-06-23 13:37:25 +0200

    Added unit tests to check the addition and editing of records

--- spring-backend/src/test/java/pl/task_manager/springbackend/TaskManagerApplicationTests.java
+++ spring-backend/src/test/java/pl/task_manager/springbackend/TaskManagerApplicationTests.java
@@ -1,13 +1,33 @@
 package pl.task_manager.springbackend;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.task_manager.springbackend.model.Task;
+import pl.task_manager.springbackend.repository.TaskRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TaskManagerApplicationTests {
+	@Autowired
+	private TaskRepository taskRepository;
 
 	@Test
-	void contextLoads() {
+	public void testSaveData() {
+		Task data = new Task(1, "test", "test description", 1, 1);
+		Task savedData = taskRepository.save(data);
+		Assertions.assertNotNull(savedData.getTitle());
+		assertEquals(data.getTitle(), savedData.getTitle());
 	}
 
+	@Test
+	public void testUpdateData() {
+		Task data = new Task(1, "test", "test description", 1, 1);
+		Task savedData = taskRepository.save(data);
+		savedData.setTitle("test2");
+		Task updatedData = taskRepository.save(savedData);
+		assertEquals(savedData.getTitle(), updatedData.getTitle());
+	}
 }
